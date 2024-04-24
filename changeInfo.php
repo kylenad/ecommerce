2b14527f4cfa03029bb15f5ea76e0f5232a5f3f0
Kyle Joseph Nadeau <kylejnadeau@gmail.com>
2024-04-23 21:08:27 -0400

    fixed center issue

--- changeInfo.php
+++ changeInfo.php
@@ -69,6 +69,7 @@ $customerInfo = getCustomerInfo($custID, $pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer Information</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
     <style>
         :root {
